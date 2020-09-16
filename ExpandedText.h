aaaeb66eb2b98134193d21b3162f8222d6af1d1c
Kre4 <denhol2002@mail.ru>
2020-09-16 21:21:49 +0300

    upgraded

--- ExpandedText.h
+++ ExpandedText.h
@@ -1,28 +1,31 @@
 //
 // Created by Денис on 14.09.2020.
 
+#pragma once
+
 #include <iostream>
 #include <stdio.h>
 #include <vector>
-#ifndef ITMO_CLUB_EXPANDEDTEXT_H
-#define ITMO_CLUB_EXPANDEDTEXT_H
-struct parameter{
-    std::string key="";
-    std::string value="";
+
+
+struct parameter {
+    std::string key = "";
+    std::string value = "";
 };
 
 class ExpandedText {
 public:
     std::string pattern;
     std::vector<parameter> params;
+
     bool fill_in(const char *path);
+
     int insert_in_pattern();
+
     void delete_empty_params();
+
     void find_current_date();
-    bool json_parser(const char  *path);
-private:
+
+    bool json_parser(const char *path);
 
 };
-
-
-#endif //ITMO_CLUB_EXPANDEDTEXT_H
